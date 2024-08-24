7a966bd249a21d69959cb07a561d1770c848a76a
Jonathan Lam <jonhaolam@gmail.com>
2024-08-24 01:04:06 -0400

    Updated layout and home page with landing page content

--- app/page.js
+++ app/page.js
@@ -1,72 +1,35 @@
-'use client'
+"use client"
+
 import Image from "next/image";
-import { useState } from "react";
-import { Box, Button, ListItemSecondaryAction, Stack, TextField } from "@mui/material";
+import Navbar from '@/components/Navbar';
+import SearchBar from "@/components/SearchBar";
+import { Container, Typography } from "@mui/material";
 
 export default function Home() {
-  const [messages, setMessages] = useState([
-    {
-      role : "model",
-      content : "Hi! I'm the rate my professor support assistant. How can I help you today?"
-    }
-  ])
-  const [message, setMessage] = useState('')
-  const sendMessage = async () => {
-    setMessages((messages) => [
-      ...messages,
-      {role : "user", content : message},
-      {role : "model", content : ''}
-    ])
-    setMessage('')
-    
-    const response = fetch('/api/chat', {
-      method : "POST",
-      headers : {
-        'Content-Type' : 'application/json',
-      },
-      body : JSON.stringify([...messages, {role : 'user', content : message}])
-    }).then(async(res) => {
-      const reader = res.body.getReader()
-      const decoder = new TextDecoder()
-
-      let result = ''
-      return reader.read().then(function processText({done, value}) {
-        if (done){
-          return result
-        }
-        const text = decoder.decode(value || new Uint8Array(), {stream : true})
-        setMessages((messages) => {
-          let lastMessage = messages[messages.length - 1]
-          let otherMessages = messages.slice(0, messages.length - 1)
-          return [
-            ...otherMessages,
-            {...lastMessage, content : lastMessage.content + text}
-          ]
-        })
-
-        return reader.read().then(processText)
-      })
-      }
-    )
-  }
   return (
-  <Box width="100vw" height="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-    <Stack direction="column" width="500px" height="700px" border="1px solid black" p={2} spacing={3}>
-      {/* chatlog */}
-      <Stack direction="column" spacing={2} flexGrow={1} overflow="auto" maxHeight="100%">
-      {messages.map((message, index) => 
-        (<Box key={index} display="flex" justifyContent={message.role === "model" ? "flex-start" : "flex-end"}>
-          <Box bgcolor={message.role ==="model" ? "primary.main" : "secondary.main"} color="white" borderRadius={16} p={3}>
-            {message.content}
-          </Box>
-        </Box>)
-      )}
-      </Stack>
-      {/* Textfield */}
-      <Stack direction="row" spacing={2}>
-        <TextField label="Message" fullWidth value={message} onChange={(e) => {setMessage(e.target.value)}} />
-        <Button variant="contained" onClick={sendMessage}>Send</Button>
-      </Stack>
-    </Stack>
-  </Box>);
+    <>
+      <Navbar />
+      <Container
+        maxWidth="100vw"
+        sx={{
+          bgcolor: "#5856D6",
+          textAlign: 'center',
+          height: 'calc(100vh - 81px)',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Typography variant="h1" component="h1" sx={{ fontWeight: 'bold', mb: 2, color: "#FFF" }}>
+          RateMate
+        </Typography>
+        <Typography variant="h5" component="h2" sx={{ color: '#E5E5E7', mb: 4 }}>
+          Your AI professor review partner.
+        </Typography>
+        <SearchBar/>
+      </Container>
+    </>
+
+  );
 }
