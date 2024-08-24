3b629a1268b2dcb283b83d44b9549d7413019766
Jonathan Lam <jonhaolam@gmail.com>
2024-08-24 01:56:10 -0400

    Modifying search page layout, added sticky button for slideout chat window

--- app/search/page.js
+++ app/search/page.js
@@ -28,67 +28,95 @@ const SearchPage = () => {
 
   return (
     <>
-        <SearchNavbar />
-        <Box sx={{ paddingTop: '80px' }}>
-            <Container maxWidth="lg">
-                
-                <Select
-                    value={subject}
-                    onChange={handleSubjectChange}
-                    displayEmpty
-                    fullWidth
-                    sx={{ marginTop: 2, marginBottom: 3 }}
-                >
-                    <MenuItem value="">
-                        <em>Select...</em>
-                    </MenuItem>
-                    <MenuItem value="Chemistry">Chemistry</MenuItem>
-                    <MenuItem value="Mathematics">Mathematics</MenuItem>
-                    <MenuItem value="Computer Science">Computer Science</MenuItem>
-                    <MenuItem value="Biology">Biology</MenuItem>
-                    <MenuItem value="Physics">Physics</MenuItem>
-                    <MenuItem value="Economics">Economics</MenuItem>
-                    <MenuItem value="Psychology">Psychology</MenuItem>
-                    <MenuItem value="English">English</MenuItem>
-                    <MenuItem value="History">History</MenuItem>
-                    <MenuItem value="Political Science">Political Science</MenuItem>
-                    <MenuItem value="Sociology">Sociology</MenuItem>
-                    <MenuItem value="Engineering">Engineering</MenuItem>
-                    <MenuItem value="Business Administration">Business Administration</MenuItem>
-                    <MenuItem value="Art">Art</MenuItem>
-                    <MenuItem value="Philosophy">Philosophy</MenuItem>
-                    <MenuItem value="Education">Education</MenuItem>
-                    <MenuItem value="Nursing">Nursing</MenuItem>
-                    <MenuItem value="Environmental Science">Environmental Science</MenuItem>
-                    <MenuItem value="Anthropology">Anthropology</MenuItem>
-                    <MenuItem value="Communications">Communications</MenuItem>
-                    <MenuItem value="Music">Music</MenuItem>
-                    <MenuItem value="Foreign Languages">Foreign Languages</MenuItem>
-                    <MenuItem value="Marketing">Marketing</MenuItem>
-                    <MenuItem value="Journalism">Journalism</MenuItem>
-                    <MenuItem value="Graphic Design">Graphic Design</MenuItem>
-                    <MenuItem value="Architecture">Architecture</MenuItem>
-                    <MenuItem value="Law">Law</MenuItem>
-                    <MenuItem value="Medicine">Medicine</MenuItem>
-                    <MenuItem value="Theater">Theater</MenuItem>
-                    <MenuItem value="Accounting">Accounting</MenuItem>
-                </Select>
+      <SearchNavbar />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: "center",
+          minHeight: '100vh',          // Makes sure the container takes up the full height of the viewport
+          padding: 2,
+        }}
+      >
+        <Box
+          sx={{
+            width: '85%',             // Adjust the width to determine how much of the page the container should take up
+            paddingTop: 15,
+          }}
+        >
+          <Box sx={{ ml: 3 }}>
+            <Typography variant="h5" >
+              {filteredReviews.length} Ratings with "<strong>{query}</strong>" in their name
+            </Typography>
+            <Box display="flex" alignItems="center" gap={2} mt={2}>
+              <Typography variant='h5'>
+                <strong>Subject</strong>
+              </Typography>
+              <Select
+                  value={subject}
+                  onChange={handleSubjectChange}
+                  displayEmpty
+                  sx={{ width: 300, marginTop: 2, marginBottom: 3 }}
+              >
+                <MenuItem value="">
+                    <em>Select...</em>
+                </MenuItem>
+                <MenuItem value="Chemistry">Chemistry</MenuItem>
+                <MenuItem value="Mathematics">Mathematics</MenuItem>
+                <MenuItem value="Computer Science">Computer Science</MenuItem>
+                <MenuItem value="Biology">Biology</MenuItem>
+                <MenuItem value="Physics">Physics</MenuItem>
+                <MenuItem value="Economics">Economics</MenuItem>
+                <MenuItem value="Psychology">Psychology</MenuItem>
+                <MenuItem value="English">English</MenuItem>
+                <MenuItem value="History">History</MenuItem>
+                <MenuItem value="Political Science">Political Science</MenuItem>
+                <MenuItem value="Sociology">Sociology</MenuItem>
+                <MenuItem value="Engineering">Engineering</MenuItem>
+                <MenuItem value="Business Administration">Business Administration</MenuItem>
+                <MenuItem value="Art">Art</MenuItem>
+                <MenuItem value="Philosophy">Philosophy</MenuItem>
+                <MenuItem value="Education">Education</MenuItem>
+                <MenuItem value="Nursing">Nursing</MenuItem>
+                <MenuItem value="Environmental Science">Environmental Science</MenuItem>
+                <MenuItem value="Anthropology">Anthropology</MenuItem>
+                <MenuItem value="Communications">Communications</MenuItem>
+                <MenuItem value="Music">Music</MenuItem>
+                <MenuItem value="Foreign Languages">Foreign Languages</MenuItem>
+                <MenuItem value="Marketing">Marketing</MenuItem>
+                <MenuItem value="Journalism">Journalism</MenuItem>
+                <MenuItem value="Graphic Design">Graphic Design</MenuItem>
+                <MenuItem value="Architecture">Architecture</MenuItem>
+                <MenuItem value="Law">Law</MenuItem>
+                <MenuItem value="Medicine">Medicine</MenuItem>
+                <MenuItem value="Theater">Theater</MenuItem>
+                <MenuItem value="Accounting">Accounting</MenuItem>
+              </Select>
+            </Box>
+          </Box>
 
-                <Typography variant="h5" gutterBottom>
-                    {filteredReviews.length} Ratings with "<strong>{query}</strong>" in their name
-                </Typography>
-
-                {filteredReviews.map((review, index) => (
-                    <ReviewCard
-                    key={index}
-                    name={review.name}
-                    subject={review.subject}
-                    stars={review.stars}
-                    review={review.review}
-                    />
-                ))}
-            </Container>
+          <Box maxWidth="70vw">
+            {filteredReviews.map((review, index) => (
+                <ReviewCard
+                key={index}
+                name={review.name}
+                subject={review.subject}
+                stars={review.stars}
+                review={review.review}
+                />
+            ))}
+          </Box>
         </Box>
+      </Box>
+      <Button
+        sx={{
+          position: 'fixed',  // Sticks the button to a fixed position on the screen
+          bottom: 20,         // 16px from the bottom of the screen
+          right: 20,          // 16px from the right side of the screen
+          zIndex: 1000,       // Ensures it stays on top of other elements
+        }}
+      >
+        Questions? Ask RateMate!
+      </Button>
     </>
   );
 };
